472ae0d64ded3eb100864d7a32c9a1df15f76a68
Sakshi_makkar <Sakshi_makkar@dell.com>
2022-12-21 13:43:55 -0500

    Option to fetch attributes by name added in volume

--- models/volume.go
+++ models/volume.go
@@ -9,14 +9,19 @@ type Volume struct {
 	Size                     types.Float64 `tfsdk:"size"`
 	CapacityUnit             types.String  `tfsdk:"capacity_unit"`
 	HostID                   types.String  `tfsdk:"host_id"`
+	HostName                 types.String  `tfsdk:"host_name"`
 	HostGroupID              types.String  `tfsdk:"host_group_id"`
+	HostGroupName            types.String  `tfsdk:"host_group_name"`
 	LogicalUnitNumber        types.Int64   `tfsdk:"logical_unit_number"`
 	VolumeGroupID            types.String  `tfsdk:"volume_group_id"`
+	VolumeGroupName          types.String  `tfsdk:"volume_group_name"`
 	MinimumSize              types.Int64   `tfsdk:"min_size"`
 	SectorSize               types.Int64   `tfsdk:"sector_size"`
 	Description              types.String  `tfsdk:"description"`
 	ApplianceID              types.String  `tfsdk:"appliance_id"`
+	ApplianceName            types.String  `tfsdk:"appliance_name"`
 	ProtectionPolicyID       types.String  `tfsdk:"protection_policy_id"`
+	ProtectionPolicyName     types.String  `tfsdk:"protection_policy_name"`
 	PerformancePolicyID      types.String  `tfsdk:"performance_policy_id"`
 	CreationTimeStamp        types.String  `tfsdk:"creation_timestamp"`
 	IsReplicationDestination types.Bool    `tfsdk:"is_replication_destination"`
