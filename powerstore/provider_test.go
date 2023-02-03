5f9dadafe64faaad81128f010ea35dbdac0518de
prajwalpatil25 <prajwal.patil@dell.com>
2023-02-03 08:57:53 -0500

    Protection policy acceptance test vars updated

--- powerstore/provider_test.go
+++ powerstore/provider_test.go
@@ -20,6 +20,10 @@ var hostGroupID = os.Getenv("HOST_GROUP_ID")
 var volumeGroupID = os.Getenv("VOLUME_GROUP_ID")
 var hostName = os.Getenv("HOST_NAME")
 var hostGroupName = os.Getenv("HOST_GROUP_NAME")
+var snapshotRuleID = os.Getenv("SNAPSHOT_RULE_ID")
+var replicationRuleID = os.Getenv("REPLICATION_RULE_ID")
+var snapshotRuleName = os.Getenv("SNAPSHOT_RULE_NAME")
+var replicationRuleName = os.Getenv("REPLICATION_RULE_NAME")
 
 func init() {
 	testProvider = New("test")()
