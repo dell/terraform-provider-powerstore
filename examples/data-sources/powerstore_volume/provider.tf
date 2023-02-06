012c0333681483c06527e3867055ca3e9620e98a
prajwalpatil25 <prajwal.patil@dell.com>
2023-02-06 07:36:44 -0500

    Resturcuting examples

---
+++ examples/data-sources/powerstore_volume/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    powerstore = {
+      version = "0.0.1"
+      source = "registry.terraform.io/dell/powerstore"
+    }
+  }
+}
+
+provider "powerstore" {
+  username = "${var.username}"
+  password = "${var.password}"
+  endpoint = "${var.endpoint}"
+  insecure = true
+}
