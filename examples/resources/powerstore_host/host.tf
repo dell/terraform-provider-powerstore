f44505bda90c7590d8aeb923633e8cc87690ed4e
Sakshi_makkar <Sakshi_makkar@dell.com>
2023-03-21 10:21:33 -0400

    added optional fields in comment

--- examples/resources/powerstore_host/host.tf
+++ examples/resources/powerstore_host/host.tf
@@ -2,10 +2,13 @@
 # Create, Update, Delete is supported for this resource
 # To import , check host_import.tf for more info
 # name,os_type and initiators are the required attributes to create and update
+# description, host_group_id and host_connectivity are the optional attributes
 # To check which attributes of the host resource can be updated, please refer Product Guide in the documentation
 
 resource "powerstore_host" "test" {
   name = "new-host1"
   os_type = "Linux"
+  description = "Creating host"
+  host_connectivity = "Local_Only"
   initiators = [{port_name= "iqn.1994-05.com.redhat:88cb605", port_type="NVMe"}]
 }
