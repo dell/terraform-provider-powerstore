c1a5601a625767edfbadc9bd0c0530d02fd8e20a
prajwalpatil25 <117063742+prajwalpatil25@users.noreply.github.com>
2023-03-21 18:28:52 +0530

    Volume Group Datasource (#50)
    * Volume Group Datasource
    
    * Fixed linting errors
    
    * Added comment
    
    * Additional acceptance tests added
    
    * Modified enpoint url in examples

--- powerstore/provider.go
+++ powerstore/provider.go
@@ -136,6 +136,7 @@ func (p *Pstoreprovider) DataSources(ctx context.Context) []func() datasource.Da
 		newVolumeDataSource,
 		newSnapshotRuleDataSource,
 		newProtectionPolicyDataSource,
+		newVolumeGroupDataSource,
 	}
 }
 
