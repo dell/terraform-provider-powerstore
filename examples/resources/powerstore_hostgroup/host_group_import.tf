bc7ae9112b1a7d1832402d1b625976ab5302d8c3
prajwalpatil25 <prajwal.patil@dell.com>
2023-03-31 11:37:46 -0400

    change in import example

--- examples/resources/powerstore_hostgroup/host_group_import.tf
+++ examples/resources/powerstore_hostgroup/host_group_import.tf
@@ -1,6 +1,6 @@
 # Below are the steps to import host group :
 # Step 1 - To import a host group, we need the id of that host group
-# Step 2 - To check the id of the host group we can make Get request to host group endpoint. eg. https://10.0.0.1/api/rest/host_group which will return list of all host group ids.
+# Step 2 - To check the id of the host group we can make use of host group datasource to read required/all host group ids. Alternatively, we can make Get request to host group endpoint. eg. https://10.0.0.1/api/rest/host_group which will return list of all host group ids.
 # Step 3 - Add empty resource block in tf file.
 # eg.
 # resource "powerstore_hostgroup" "resource_block_name" {
