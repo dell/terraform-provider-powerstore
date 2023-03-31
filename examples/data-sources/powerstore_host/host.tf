2dc24135991921fb9b51e0ea83ff316f577bcbee
Sakshi_makkar <Sakshi_makkar@dell.com>
2023-03-31 07:33:48 -0400

    Host DataSource Added

---
+++ examples/data-sources/powerstore_host/host.tf
@@ -0,0 +1,13 @@
+# commands to run this tf file : terraform init && terraform apply --auto-approve
+# This datasource reads hosts either by id or name where user can provide a value to any one of them
+# If it is a empty datsource block , then it will read all the hosts
+# If id or name is provided then it reads a particular host with that id or name
+# Only one of the attribute can be provided among id and name 
+
+data "powerstore_host" "test1" {
+        name = "tf_host"
+}
+
+output "hostResult" {
+  value = data.powerstore_host.test1.hosts
+}
