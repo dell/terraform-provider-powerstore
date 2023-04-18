39669e90c1f7deb5655cf77714efefb32a20d639
Sakshi_makkar <Sakshi_makkar@dell.com>
2023-04-18 03:43:08 -0400

    tf file updated

--- examples/resources/powerstore_volumeGroupSnapshot/volumeGroupSnapshot.tf
+++ examples/resources/powerstore_volumeGroupSnapshot/volumeGroupSnapshot.tf
@@ -2,7 +2,7 @@
 # Create, Update, Delete is supported for this resource
 # To import , check volumeGroupSnapshot_import.tf for more info
 # name, volume_group_id/volume_group_name and expiration_timestamp are the required attributes to create and update
-# description is the optional attributes
+# description is the optional attribute
 # Either volume_group_id or volume_group_name should be present.
 # To check which attributes of the volume group snapshot resource can be updated, please refer Product Guide in the documentation
 
