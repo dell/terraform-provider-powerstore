5c143a8d1c913ccfb960c0ec030eb4b2cece44c1
Sakshi_makkar <Sakshi_makkar@dell.com>
2023-05-22 06:11:08 -0400

    Volumegroup Snapshot DataSource Added

---
+++ powerstore/datasource_volume_group_snapshot.go
@@ -0,0 +1,258 @@
+package powerstore
+
+import (
+	"context"
+	"github.com/dell/gopowerstore"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"terraform-provider-powerstore/client"
+)
+
+type volumeGroupSnapshotDataSource struct {
+	client *client.Client
+}
+
+var (
+	_ datasource.DataSource              = &volumeGroupSnapshotDataSource{}
+	_ datasource.DataSourceWithConfigure = &volumeGroupSnapshotDataSource{}
+)
+
+// newVolumeGroupSnapshotDataSource returns the volume group data source object
+func newVolumeGroupSnapshotDataSource() datasource.DataSource {
+	return &volumeGroupSnapshotDataSource{}
+}
+
+func (d *volumeGroupSnapshotDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_volumegroup_snapshot"
+}
+
+func (d *volumeGroupSnapshotDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "VolumeGroup DataSource.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description:         "Unique identifier of the volume group. Conflicts with `name`.",
+				MarkdownDescription: "Unique identifier of the volume group. Conflicts with `name`.",
+				Optional:            true,
+				Computed:            true,
+				Validators: []validator.String{
+					stringvalidator.ConflictsWith(path.MatchRoot("name")),
+					stringvalidator.LengthAtLeast(1),
+				},
+			},
+			"name": schema.StringAttribute{
+				Description:         "Volume group name. Conflicts with `id`.",
+				MarkdownDescription: "Volume group name. Conflicts with `id`.",
+				Optional:            true,
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+				},
+			},
+
+			"volume_groups": schema.ListNestedAttribute{
+				Description:         "List of volume groups.",
+				MarkdownDescription: "List of volume groups.",
+				Computed:            true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Description:         "Unique identifier of the volume group.",
+							MarkdownDescription: "Unique identifier of the volume group.",
+							Computed:            true,
+						},
+						"name": schema.StringAttribute{
+							Description:         "Volume group name.",
+							MarkdownDescription: "Volume group name.",
+							Computed:            true,
+						},
+						"description": schema.StringAttribute{
+							Description:         "Volume group description.",
+							MarkdownDescription: "Volume group description.",
+							Computed:            true,
+						},
+						"creation_timestamp": schema.StringAttribute{
+							Description:         "The time at which the volume group was created.",
+							MarkdownDescription: "The time at which the volume group was created.",
+							Computed:            true,
+						},
+						"is_protectable": schema.BoolAttribute{
+							Description:         "This is a derived field that is set internally.",
+							MarkdownDescription: "This is a derived field that is set internally.",
+							Computed:            true,
+						},
+						"protection_policy_id": schema.StringAttribute{
+							Description:         "Unique identifier of the protection policy assigned to the volume group.",
+							MarkdownDescription: "Unique identifier of the protection policy assigned to the volume group.",
+							Computed:            true,
+						},
+						"migration_session_id": schema.StringAttribute{
+							Description:         "Unique identifier of the migration session assigned to the volume group when it is part of a migration activity.",
+							MarkdownDescription: "Unique identifier of the migration session assigned to the volume group when it is part of a migration activity.",
+							Computed:            true,
+						},
+						"is_write_order_consistent": schema.BoolAttribute{
+							Description:         "For a primary or a clone volume group, this property determines whether snapshot sets of the group will be write order consistent.",
+							MarkdownDescription: "For a primary or a clone volume group, this property determines whether snapshot sets of the group will be write order consistent.",
+							Computed:            true,
+						},
+						"placement_rule": schema.StringAttribute{
+							Description:         "This is set during creation, and determines resource balancer recommendations.",
+							MarkdownDescription: "This is set during creation, and determines resource balancer recommendations.",
+							Computed:            true,
+						},
+						"type": schema.StringAttribute{
+							Description:         "Type of volume.",
+							MarkdownDescription: "Type of volume.",
+							Computed:            true,
+						},
+						"is_replication_destination": schema.BoolAttribute{
+							Description:         "Indicates whether this volume group is a replication destination.",
+							MarkdownDescription: "Indicates whether this volume group is a replication destination.",
+							Computed:            true,
+						},
+						"protection_data": schema.ObjectAttribute{
+							Description:         "Specifies the ProtectionData associated with a volume group.",
+							MarkdownDescription: "Specifies the ProtectionData associated with a volume group.",
+							Computed:            true,
+							AttributeTypes: map[string]attr.Type{
+								"source_id": types.StringType,
+							},
+						},
+						"is_importing": schema.BoolAttribute{
+							Description:         "Indicates whether the volume group is being imported.",
+							MarkdownDescription: "Indicates whether the volume group is being imported.",
+							Computed:            true,
+						},
+						"location_history": schema.ListNestedAttribute{
+							Description:         "Storage resource location history.",
+							MarkdownDescription: "Storage resource location history.",
+							Computed:            true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"from_appliance_id": schema.StringAttribute{
+										Description:         "Unique identifier of the appliance from which the volume was relocated.",
+										MarkdownDescription: "Unique identifier of the appliance from which the volume was relocated.",
+										Computed:            true,
+									},
+									"to_appliance_id": schema.StringAttribute{
+										Description:         "Unique identifier of the appliance to which the volume was relocated.",
+										MarkdownDescription: "Unique identifier of the appliance to which the volume was relocated.",
+										Computed:            true,
+									},
+								},
+							},
+						},
+						"type_l10n": schema.StringAttribute{
+							Description:         "Localized message string corresponding to type",
+							MarkdownDescription: "Localized message string corresponding to type",
+							Computed:            true,
+						},
+						"protection_policy": schema.ObjectAttribute{
+							Description:         "Specifies the Protection Policy associated with a volume group.",
+							MarkdownDescription: "Specifies the Protection Policy associated with a volume group.",
+							Computed:            true,
+							AttributeTypes: map[string]attr.Type{
+								"id":          types.StringType,
+								"name":        types.StringType,
+								"description": types.StringType,
+							},
+						},
+						"migration_session": schema.ObjectAttribute{
+							Description:         "Properties of a migration session.",
+							MarkdownDescription: "Properties of a migration session.",
+							Computed:            true,
+							AttributeTypes: map[string]attr.Type{
+								"id":   types.StringType,
+								"name": types.StringType,
+							},
+						},
+						"volumes": schema.ListNestedAttribute{
+							Description:         "List of the volumes that are associated with this volume_group.",
+							MarkdownDescription: "List of the volumes that are associated with this volume_group.",
+							Computed:            true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"id": schema.StringAttribute{
+										Description:         "Unique identifier of the volume instance.",
+										MarkdownDescription: "Unique identifier of the volume instance.",
+										Computed:            true,
+									},
+									"name": schema.StringAttribute{
+										Description:         "Name of the volume.",
+										MarkdownDescription: "Name of the volume.",
+										Computed:            true,
+									},
+									"description": schema.StringAttribute{
+										Description:         "Description of the volume.",
+										MarkdownDescription: "Description of the volume.",
+										Computed:            true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (d *volumeGroupSnapshotDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, _ *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+	d.client = req.ProviderData.(*client.Client)
+}
+
+func (d *volumeGroupSnapshotDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state volumeGroupDataSourceModel
+	var volumeGroups []gopowerstore.VolumeGroup
+	var volumeGroup gopowerstore.VolumeGroup
+	var err error
+
+	diags := req.Config.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	//Read the volume group based on volume group id/name and if nothing is mentioned, then it returns all the volume groups
+	if state.Name.ValueString() != "" {
+		volumeGroup, err = d.client.PStoreClient.GetVolumeGroupSnapshotByName(context.Background(), state.Name.ValueString())
+		volumeGroups = append(volumeGroups, volumeGroup)
+	} else if state.ID.ValueString() != "" {
+		volumeGroup, err = d.client.PStoreClient.GetVolumeGroupSnapshot(context.Background(), state.ID.ValueString())
+		volumeGroups = append(volumeGroups, volumeGroup)
+	} else {
+		volumeGroups, err = d.client.PStoreClient.GetVolumeGroupSnapshots(context.Background())
+	}
+
+	//check if there is any error while getting the volume group
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Read PowerStore Volume Group",
+			err.Error(),
+		)
+		return
+	}
+
+	state.VolumeGroups, err = updateVolGroupState(volumeGroups, d.client)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Failed to update volume group state",
+			err.Error(),
+		)
+		return
+	}
+	state.ID = types.StringValue("placeholder")
+	diags = resp.State.Set(ctx, state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
---
+++ powerstore/datasource_volume_group_snapshot_test.go
@@ -0,0 +1,100 @@
+package powerstore
+
+import (
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+// Test to Fetch Volume Groups
+func TestAccVolumeGroupSnapshot_FetchVolumeGroupSnapshot(t *testing.T) {
+	if os.Getenv("TF_ACC") == "" {
+		t.Skip("Dont run with units tests because it will try to create the context")
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testProviderFactory,
+		Steps: []resource.TestStep{
+			{
+				Config: ProviderConfigForTesting + VolumeGroupSnapshotDataSourceparamsName,
+			},
+			{
+				Config: ProviderConfigForTesting + VolumeGroupSnapshotDataSourceparamsID,
+			},
+			{
+				Config: ProviderConfigForTesting + VolumeGroupSnapshotDataSourceparamsAll,
+			},
+			{
+				Config:      ProviderConfigForTesting + VolumeGroupSnapshotDataSourceparamsIDAndNameNegative,
+				ExpectError: regexp.MustCompile("Invalid Attribute Combination"),
+			},
+			{
+				Config:      ProviderConfigForTesting + VolumeGroupSnapshotDataSourceparamsEmptyIDNegative,
+				ExpectError: regexp.MustCompile("Invalid Attribute Value Length"),
+			},
+			{
+				Config:      ProviderConfigForTesting + VolumeGroupSnapshotDataSourceparamsEmptyNameNegative,
+				ExpectError: regexp.MustCompile("Invalid Attribute Value Length"),
+			},
+			{
+				Config:      ProviderConfigForTesting + VolumeGroupSnapshotDataSourceparamsNameNegative,
+				ExpectError: regexp.MustCompile("Unable to Read PowerStore Volume Group"),
+			},
+			{
+				Config:      ProviderConfigForTesting + VolumeGroupSnapshotDataSourceparamsIDNegative,
+				ExpectError: regexp.MustCompile("Unable to Read PowerStore Volume Group"),
+			},
+		},
+	})
+}
+
+var VolumeGroupSnapshotDataSourceparamsName = `
+data "powerstore_volumegroup_snapshot" "test1" {
+	name = "` + volumeGroupSnapshotName + `"
+}
+`
+
+var VolumeGroupSnapshotDataSourceparamsNameNegative = `
+data "powerstore_volumegroup_snapshot" "test1" {
+	name = "invalid-name"
+}
+`
+
+var VolumeGroupSnapshotDataSourceparamsID = `
+data "powerstore_volumegroup_snapshot" "test1" {
+	id = "` + volumeGroupSnapshotID + `"
+}
+`
+
+var VolumeGroupSnapshotDataSourceparamsAll = `
+data "powerstore_volumegroup_snapshot" "test1" {
+}
+`
+
+var VolumeGroupSnapshotDataSourceparamsIDNegative = `
+data "powerstore_volumegroup_snapshot" "test1" {
+	id = "invalid-id"
+}
+`
+
+var VolumeGroupSnapshotDataSourceparamsIDAndNameNegative = `
+data "powerstore_volumegroup_snapshot" "test1" {
+	id = "` + volumeGroupSnapshotID + `"
+	name = "` + volumeGroupSnapshotName + `"
+}
+`
+
+var VolumeGroupSnapshotDataSourceparamsEmptyIDNegative = `
+data "powerstore_volumegroup_snapshot" "test1" {
+	id = ""
+}
+`
+
+var VolumeGroupSnapshotDataSourceparamsEmptyNameNegative = `
+data "powerstore_volumegroup_snapshot" "test1" {
+	name = ""
+}
+`
