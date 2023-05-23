2f8cc94fc2743ed84d443aaf004fed7b2cf77724
Sakshi_makkar <Sakshi_makkar@dell.com>
2023-05-23 07:19:38 -0400

    host resource documentation added

--- examples/resources/powerstore_host/resource.tf
+++ examples/resources/powerstore_host/resource.tf
@@ -1,7 +1,7 @@
 # Commands to run this tf file : terraform init && terraform plan && terraform apply
 # Create, Update, Delete is supported for this resource
 # To import , check host_import.tf for more info
-# name,os_type and initiators are the required attributes to create and update
+# name, os_type and initiators are the required attributes to create and update
 # description and host_connectivity are the optional attributes
 # To check which attributes of the host resource can be updated, please refer Product Guide in the documentation
 
@@ -10,5 +10,5 @@ resource "powerstore_host" "test" {
   os_type           = "Linux"
   description       = "Creating host"
   host_connectivity = "Local_Only"
-  initiators        = [{ port_name = "iqn.1994-05.com.redhat:88cb605", port_type = "NVMe" }]
+  initiators        = [{ port_name = "iqn.1994-05.com.redhat:88cb605", port_type = "iSCSI" }]
 }
