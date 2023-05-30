59b07fd5010757168fa4e3f006aa2e8ff136ab17
Sakshi_makkar <Sakshi_makkar@dell.com>
2023-05-30 02:33:31 -0400

    comment addressed

--- examples/resources/powerstore_volumegroup_snapshot/resource.tf
+++ examples/resources/powerstore_volumegroup_snapshot/resource.tf
@@ -4,9 +4,9 @@
 # name and volume_group_id/volume_group_name are the required attributes to create volume group snapshot.
 # description and expiration_timestamp are the optional attributes.
 # Either volume_group_id or volume_group_name should be present.
-# VolumeGroup DataSource can be used to fetch volume group ID/Name
-# During create operation, if expiration_timestamp is not specified or set to blank, snapshot will be created with infinite retention
-# During modify operation, to set infinite retention, expiration_timestamp can be set to blank
+# VolumeGroup DataSource can be used to fetch volume group ID/Name.
+# During create operation, if expiration_timestamp is not specified or set to blank(""), snapshot will be created with infinite retention.
+# During modify operation, to set infinite retention, expiration_timestamp can be set to blank("").
 # To check which attributes of the volume group snapshot resource can be updated, please refer Product Guide in the documentation
 
 resource "powerstore_volumegroup_snapshot" "test" {
