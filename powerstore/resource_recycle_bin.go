e375fc0a285380996fca854d85347b77f3fc1937
Saksham Nautiyal <Saksham.nautiyal@dell.com>
2026-05-19 09:25:55 -0400

    fix: Change resource type name from recycle_bin_config to recycle_bin
    QE tests use powerstore_recycle_bin but the resource was registered as
    powerstore_recycle_bin_config, causing 'Invalid resource type' errors.

--- powerstore/resource_recycle_bin.go
+++ powerstore/resource_recycle_bin.go
@@ -48,7 +48,7 @@ type resourceRecycleBin struct {
 
 // Metadata defines resource interface Metadata method
 func (r *resourceRecycleBin) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_recycle_bin_config"
+	resp.TypeName = req.ProviderTypeName + "_recycle_bin"
 }
 
 // Schema defines resource interface Schema method
