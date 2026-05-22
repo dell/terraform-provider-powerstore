fe0a07f20686fb701d1ec65e121b888a55cae5d4
Saksham Nautiyal <Saksham.nautiyal@dell.com>
2026-05-22 03:35:38 -0400

    Fix formatting issues in metro files

--- models/metro.go
+++ models/metro.go
@@ -21,30 +21,30 @@ import "github.com/hashicorp/terraform-plugin-framework/types"
 
 // MetroVolumeResource - Terraform state model for metro_volume resource
 type MetroVolumeResource struct {
-	ID                         types.String `tfsdk:"id"`
-	VolumeID                   types.String `tfsdk:"volume_id"`
-	RemoteSystemID             types.String `tfsdk:"remote_system_id"`
-	RemoteApplianceID          types.String `tfsdk:"remote_appliance_id"`
-	DeleteRemoteVolume         types.Bool   `tfsdk:"delete_remote_volume"`
-	Force                      types.Bool   `tfsdk:"force"`
-	State                      types.String `tfsdk:"state"`
-	RemoteResourceID           types.String `tfsdk:"remote_resource_id"`
-	DataTransferState          types.String `tfsdk:"data_transfer_state"`
-	MetroReplicationSessionID  types.String `tfsdk:"metro_replication_session_id"`
+	ID                        types.String `tfsdk:"id"`
+	VolumeID                  types.String `tfsdk:"volume_id"`
+	RemoteSystemID            types.String `tfsdk:"remote_system_id"`
+	RemoteApplianceID         types.String `tfsdk:"remote_appliance_id"`
+	DeleteRemoteVolume        types.Bool   `tfsdk:"delete_remote_volume"`
+	Force                     types.Bool   `tfsdk:"force"`
+	State                     types.String `tfsdk:"state"`
+	RemoteResourceID          types.String `tfsdk:"remote_resource_id"`
+	DataTransferState         types.String `tfsdk:"data_transfer_state"`
+	MetroReplicationSessionID types.String `tfsdk:"metro_replication_session_id"`
 }
 
 // MetroVolumeGroupResource - Terraform state model for metro_volume_group resource
 type MetroVolumeGroupResource struct {
-	ID                            types.String `tfsdk:"id"`
-	VolumeGroupID                 types.String `tfsdk:"volume_group_id"`
-	RemoteSystemID                types.String `tfsdk:"remote_system_id"`
-	RemoteApplianceID             types.String `tfsdk:"remote_appliance_id"`
-	DeleteRemoteVolumeGroup       types.Bool   `tfsdk:"delete_remote_volume_group"`
-	Force                         types.Bool   `tfsdk:"force"`
-	State                         types.String `tfsdk:"state"`
-	RemoteResourceID              types.String `tfsdk:"remote_resource_id"`
-	DataTransferState             types.String `tfsdk:"data_transfer_state"`
-	MetroReplicationSessionID     types.String `tfsdk:"metro_replication_session_id"`
+	ID                        types.String `tfsdk:"id"`
+	VolumeGroupID             types.String `tfsdk:"volume_group_id"`
+	RemoteSystemID            types.String `tfsdk:"remote_system_id"`
+	RemoteApplianceID         types.String `tfsdk:"remote_appliance_id"`
+	DeleteRemoteVolumeGroup   types.Bool   `tfsdk:"delete_remote_volume_group"`
+	Force                     types.Bool   `tfsdk:"force"`
+	State                     types.String `tfsdk:"state"`
+	RemoteResourceID          types.String `tfsdk:"remote_resource_id"`
+	DataTransferState         types.String `tfsdk:"data_transfer_state"`
+	MetroReplicationSessionID types.String `tfsdk:"metro_replication_session_id"`
 }
 
 // ReplicationSessionActionResource - Terraform state model for replication_session_action resource
@@ -59,28 +59,28 @@ type ReplicationSessionActionResource struct {
 
 // ReplicationSessionDataSource - Terraform state model for replication_session data source
 type ReplicationSessionDataSource struct {
-	ID                  types.String                   `tfsdk:"id"`
-	ReplicationSessions []ReplicationSessionItemModel  `tfsdk:"replication_sessions"`
+	ID                  types.String                  `tfsdk:"id"`
+	ReplicationSessions []ReplicationSessionItemModel `tfsdk:"replication_sessions"`
 }
 
 // ReplicationSessionItemModel - Terraform state model for a single replication session item
 type ReplicationSessionItemModel struct {
-	ID                    types.String `tfsdk:"id"`
-	State                 types.String `tfsdk:"state"`
-	Role                  types.String `tfsdk:"role"`
-	ResourceType          types.String `tfsdk:"resource_type"`
-	DataTransferState     types.String `tfsdk:"data_transfer_state"`
-	Type                  types.String `tfsdk:"type"`
-	LastSyncTimestamp     types.String `tfsdk:"last_sync_timestamp"`
-	LocalResourceID       types.String `tfsdk:"local_resource_id"`
-	RemoteResourceID      types.String `tfsdk:"remote_resource_id"`
-	RemoteSystemID        types.String `tfsdk:"remote_system_id"`
-	ProgressPercentage    types.Int64  `tfsdk:"progress_percentage"`
-	ReplicationRuleID     types.String `tfsdk:"replication_rule_id"`
-	LastSyncDuration      types.Int64  `tfsdk:"last_sync_duration"`
-	FailoverTestInProgress types.Bool  `tfsdk:"failover_test_in_progress"`
-	StateL10n             types.String `tfsdk:"state_l10n"`
-	RoleL10n              types.String `tfsdk:"role_l10n"`
-	ResourceTypeL10n      types.String `tfsdk:"resource_type_l10n"`
-	TypeL10n              types.String `tfsdk:"type_l10n"`
+	ID                     types.String `tfsdk:"id"`
+	State                  types.String `tfsdk:"state"`
+	Role                   types.String `tfsdk:"role"`
+	ResourceType           types.String `tfsdk:"resource_type"`
+	DataTransferState      types.String `tfsdk:"data_transfer_state"`
+	Type                   types.String `tfsdk:"type"`
+	LastSyncTimestamp      types.String `tfsdk:"last_sync_timestamp"`
+	LocalResourceID        types.String `tfsdk:"local_resource_id"`
+	RemoteResourceID       types.String `tfsdk:"remote_resource_id"`
+	RemoteSystemID         types.String `tfsdk:"remote_system_id"`
+	ProgressPercentage     types.Int64  `tfsdk:"progress_percentage"`
+	ReplicationRuleID      types.String `tfsdk:"replication_rule_id"`
+	LastSyncDuration       types.Int64  `tfsdk:"last_sync_duration"`
+	FailoverTestInProgress types.Bool   `tfsdk:"failover_test_in_progress"`
+	StateL10n              types.String `tfsdk:"state_l10n"`
+	RoleL10n               types.String `tfsdk:"role_l10n"`
+	ResourceTypeL10n       types.String `tfsdk:"resource_type_l10n"`
+	TypeL10n               types.String `tfsdk:"type_l10n"`
 }
